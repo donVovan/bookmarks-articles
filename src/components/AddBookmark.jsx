f36462d0b79c26faa35f32f766450d4c51793bf5
Vladimir Blinov <vladimir.a.blinov@gmail.com>
2023-11-15 15:44:24 +0300

    Подключил базу MongoDB

--- src/components/AddBookmark.jsx
+++ src/components/AddBookmark.jsx
@@ -1,7 +1,62 @@
+import {useState, useEffect} from "react";
+import data from '../data/bookmark.json'
+
 function AddBookmark() {
-    return <div>
-        Добавить ссылку
-    </div>
+    const [link, setLink] = useState('');
+    const [title, setTitle] = useState('');
+    const [jsonData, setJsonData] = useState('');
+
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    async function fetchData() {
+        const response = await fetch('http://localhost:3002/test');
+        const data = await response.json();
+        setJsonData(data);
+    }
+
+    useEffect(() => {
+        //console.log(jsonData)
+    }, [jsonData]);
+
+
+    function handleAddBookmark() {
+        const newBookmark = {link, title};
+
+        const existingBookmarks = data.bookmarks || [];
+
+        localStorage.setItem('bookmarks', JSON.stringify([...existingBookmarks, newBookmark]))
+
+        setLink('');
+        setTitle('');
+    }
+
+
+    return (
+        <div>
+            <form>
+                <label>
+                    Link: <input
+                    type="text"
+                    value={link}
+                    onChange={event => setLink(event.target.value)}
+                />
+                </label>
+            </form>
+            <form>
+                <label>
+                    Tittle: <input
+                    type="text"
+                    value={title}
+                    onChange={event => setTitle(event.target.value)}
+                />
+                </label>
+            </form>
+            <button onClick={handleAddBookmark}>Add Bookmark</button>
+        </div>
+    )
 }
 
 export default AddBookmark;
--- src/components/NewBookmark.jsx
+++ src/components/NewBookmark.jsx
@@ -1,6 +1,27 @@
+import {useEffect, useState} from "react";
+
 function NewBookmark() {
+    const [jsonData, setJsonData] = useState('');
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    async function fetchData() {
+        const response = await fetch('http://localhost:3002/test');
+        const data = await response.json();
+        setJsonData(data);
+    }
+
+    console.log(jsonData)
+
+/*    useEffect(() => {
+        //let res = typeof jsonData
+        //console.log(res)
+    }, [jsonData]);*/
+
     return <div>
-        Непрочитанные ссылки
+        {}
     </div>
 }
 
