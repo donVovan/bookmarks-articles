cae3f0908afe245638105c46d7933c5d28c37ee7
Vladimir Blinov <vladimir.a.blinov@gmail.com>
2023-11-30 12:31:04 +0300

    добавил подробные комментарии

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -4,55 +4,67 @@ import {useEffect, useState} from "react";
 import axios from "axios";
 
 function Container() {
-    const [bookmark, setBookmark] = useState({
+    const [bookmark, setBookmark] = useState({ // определение состояния bookmark и функции для его обновления
         link:'',
         title:'',
         status: true
     })
-    const [jsonData, setJsonData] = useState(null);
+    const [jsonData, setJsonData] = useState(null); //определение состояния jsonData (JSON-данные) и функции для его обновления
 
-    useEffect(() => {
-        axios.get('http://localhost:3002/test/')
-            .then(response => {
-                setJsonData(response.data)
+    useEffect(() => { // эффект, который срабатывает после первого рендера компонента
+        axios.get('http://localhost:3002/test/') // выполнение GET-запроса к серверу для получения данных
+            .then(response => { // обработка успешного ответа от сервера
+                setJsonData(response.data) // обновление состояния jsonData данными из ответа
             })
             .catch(error => {
                 console.error('Ошибка получения данных:', error)
             })
-    }, [])
+    }, [])// зависимость пустая, поэтому эффект срабатывает только один раз после монтирования компонента
 
-    function handleStatus(index) {
-        const updatedData = jsonData.map((item, i) => {
-            if (i === index){
-                return {...item, status: false};
+    function handleStatus(index) { // функция для обновления статуса элемента по индексу
+        const updatedData = jsonData.map((item, i) => { // обновление данных: создание нового массива с обновленным элементом
+            if (i === index){ // если индекс соответствует переданному
+                return {...item, status: false}; // возвращаем обновлённый элемент со статусом false
             }
-            return item;
+            return item; // иначе возвращаем элемент без изменений
         });
-        setJsonData(updatedData);
-        console.log(updatedData)
+        setJsonData(updatedData); // обновление состояния jsonData новыми данными
+        console.log(jsonData)
+    }
+
+    async function handleButtonClick() { // асинхронная функция для обработки клика по кнопке
+        try {
+            const response = await axios.post('http://localhost:3002/data/', jsonData); // отправка POST-запроса с данными на сервер
+            console.log('Данные успешно отправлены на сервер:', response.data);
+            // Логика обновления UI или другие действия после успешной отправки
+        } catch (error) {
+            console.error('Ошибка при отправке данных на сервер:', error);
+            // Логика обработки ошибки, если необходимо
+        }
     }
 
 
-    function renderDataNew() {
+    function renderDataNew() { // функция для отображения новых закладок
         if (jsonData){
             return (
                 <>
                 <ul>
-                    {jsonData.map((item, index) => (
-                        item.status && (
+                    {jsonData.map((item, index) => ( //маппинг данных для отображения в виде списка
+                        item.status && ( // если статус равен true
+                            //создаем элемент списка с уникальным ключом
                             <li key={item._id}>
                                 <a href={item.link}>{item.title}</a>
                                 <input
                                     type="checkbox"
                                     checked={item.checked}
-                                    onChange={() => handleStatus(index)}
+                                    onChange={() => handleStatus(index)} // обработчик изменения состояния при клике на чекбокс
                                 />
                             </li>
                         )
                         )
                     )}
                 </ul>
-                    <button>Сохранить изменения</button>
+                    <button onClick={handleButtonClick}>Сохранить изменения</button>
                 </>
             )
         }
@@ -60,11 +72,11 @@ function Container() {
 
 
 
-    async function handleAddBookmark(event) {
-        event.preventDefault();
+    async function handleAddBookmark(event) { // асинхронная функция для добавления закладки
+        event.preventDefault(); // предотвращение действия по умолчанию при отправке формы
 
-        axios.post('http://localhost:3002/add/', bookmark)
-            .then(response => {
+        axios.post('http://localhost:3002/add/', bookmark) // отправка POST-запроса с данными о закладке на сервер
+            .then(response => { // обработка успешного ответа от сервера
                 console.log('Данные успешно отправлены на сервер:', response.data);
                 // Опционально: обновление состояния или выполнение других действий после отправки
             })
