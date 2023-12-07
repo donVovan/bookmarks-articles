6d7151f584e91577c46a2e3dffab26840ef33e8e
Vladimir Blinov <vladimir.a.blinov@gmail.com>
2023-12-07 17:20:09 +0300

    реализовал удаление записей из старых закладок

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -10,11 +10,10 @@ function Container() {
         title:'',
         status: true
     })
+    const [jsonData, setJsonData] = useState(null); //определение состояния jsonData (JSON-данные) и функции для его обновления
+    const [deletedItems, setDeletedItems] = useState([]); // определение состояния для хранения удаленных элементов
 
     console.log(bookmark)
-
-    const [jsonData, setJsonData] = useState(null); //определение состояния jsonData (JSON-данные) и функции для его обновления
-
     useEffect(() => { // эффект, который срабатывает после первого рендера компонента
         axios.get('http://localhost:3002/test/') // выполнение GET-запроса к серверу для получения данных
             .then(response => { // обработка успешного ответа от сервера
@@ -36,13 +35,33 @@ function Container() {
         console.log(jsonData)
     }
 
-    async function handleButtonClick() { // асинхронная функция для обработки клика по кнопке
+    function removeItem(index) {
+        const deletedItem = jsonData[index]; // сохраняем ссылку на удаленный элемент
+        setDeletedItems(prev => [...prev, deletedItem]); // добавляем удаленный элемент в состояние deletedItems
+        const updatedData = jsonData.filter((item, i) => i !== index); // фильтрация данных для убирания удаленного элемента
+        setJsonData(updatedData); // обновление состояния jsonData новыми данными
+    }
+
+    async function handleButtonClickMove() { // асинхронная функция для обработки клика по кнопке
         try {
             const response = await axios.post('http://localhost:3002/data/', jsonData); // отправка POST-запроса с данными на сервер
             console.log('Данные успешно отправлены на сервер:', response.data);
             // Логика обновления UI или другие действия после успешной отправки
         } catch (error) {
             console.error('Ошибка при отправке данных на сервер:', error);
+            // Логика обработки ошибки, если необходимо
+        }
+    }
+
+    async function handleButtonClickDel() {
+        try {
+            await Promise.all(deletedItems.map(async (item) => {
+                const response = await axios.delete(`http://localhost:3002/delete/${item._id}`); // отправка DELETE-запроса для удаления элемента по ID на сервер
+                console.log('Элемент успешно удален с сервера:', response.data);
+            }));
+            setDeletedItems([]);// очищаем состояние deletedItems после успешного удаления
+        } catch (error) {
+            console.error('Ошибка при удалении данных на сервере:', error);
             // Логика обработки ошибки, если необходимо
         }
     }
@@ -68,7 +87,7 @@ function Container() {
                         )
                     )}
                 </ul>
-                    <button onClick={handleButtonClick}>Сохранить изменения</button>
+                    <button onClick={handleButtonClickMove}>Сохранить изменения</button>
                 </>
             )
         }
@@ -87,14 +106,14 @@ function Container() {
                                         <input
                                             type="checkbox"
                                             checked={item.checked}
-                                            onChange={() => handleStatus(index)} // обработчик изменения состояния при клике на чекбокс
+                                            onChange={() => removeItem(index)} // обработчик изменения состояния при клике на чекбокс
                                         />
                                     </li>
                                 )
                             )
                         )}
                     </ul>
-                    <button onClick={handleButtonClick}>Сохранить изменения</button>
+                    <button onClick={handleButtonClickDel}>Сохранить изменения</button>
                 </>
             )
         }
